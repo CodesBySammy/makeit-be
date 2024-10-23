b807d760ea5abd5152455d2d423e52659303b45b
Sameer Mahindru <113050438+CodesBySammy@users.noreply.github.com>
2024-10-24 02:50:51 +0530

    Delete models directory

--- models/User.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const userSchema = new mongoose.Schema({
-  name: String,
-  email: { type: String, unique: true },
-  phone: { type: String, unique: true },
-}, { timestamps: true });
-
-module.exports = mongoose.models.User || mongoose.model('User', userSchema);
