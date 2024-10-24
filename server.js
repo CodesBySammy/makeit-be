2498487b347224207b8ea79d1bf825bb2e57ae0c
Sameer Mahindru <113050438+CodesBySammy@users.noreply.github.com>
2024-10-24 22:07:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -101,5 +101,5 @@ app.post('/api/download', async (req, res) => {
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(Server is running on port ${PORT});
+    console.log('Server is running on port ${PORT}');
 });
